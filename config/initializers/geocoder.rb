e73072c17779b59d48dc3eaf7be6533eeb0379c0
Max Kramer <mckramer@gmail.com>
2012-06-15 14:02:49 -0400

    Clean up

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,2 +1,21 @@
-Geocoder::Configuration.http_proxy = "http.proxy.fmr.com:8000"
-Geocoder::Configuration.timeout = 5
+Geocoder.configure do |config|
+
+  # geocoding service (see below for supported options):
+  # config.lookup = :yahoo
+
+  # to use an API key:
+  # config.api_key = "..."
+  
+  config.http_proxy = 'http://http.proxy.fmr.com:8000'
+
+  # geocoding service request timeout, in seconds (default 3):
+  # config.timeout = 5
+
+  # set default units to kilometers:
+  # config.units = :km
+
+  # caching (see below for details):
+  # config.cache = Redis.new
+  # config.cache_prefix = "..."
+
+end
